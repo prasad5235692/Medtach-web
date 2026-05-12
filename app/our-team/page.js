ea9abf547b8336e8dfacbc4a63990a7dc915fe7f
Prasad <pk5235692@gmail.com>
2026-05-12 13:19:20 +0530

    update cols

--- app/our-team/page.js
+++ app/our-team/page.js
@@ -68,7 +68,7 @@ export default async function OurTeamPage() {
               subtitle={content.managementSection.subtitle}
             />
           </AnimateOnScroll>
-          <div className="mt-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          <div className="mt-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2">
             {content.managementSection.members.map((member, i) => (
               <AnimateOnScroll key={member.id} animation="fade-up" delay={i * 100}>
                 <TeamCard member={member} showBio />
